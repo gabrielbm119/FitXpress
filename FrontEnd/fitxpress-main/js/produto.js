0792d3cbe1fce75611b0fe2e9bc8a53fc603f2c9
Jude Vieira <judevieiradlima@gmail.com>
2025-07-31 17:14:30 -0300

    commit-dia3107

---
+++ FrontEnd/fitxpress-main/js/produto.js
@@ -0,0 +1,9 @@
+function produtoSelecionado(id){
+    const id = localStorage.getItem("produtoSelecionado");
+    const produto = produtos.find(p => p.id === id);
+
+    if(produto){
+        document.getElementById("")
+    }
+
+}
